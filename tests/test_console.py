045b9756e20b96f113bce04c7eefdd4dd4428065
Eunice Adewusi <e.adewusi@alustudent.com>
2024-01-27 00:31:44 +0100

    resolve syntax error

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,80 +1,295 @@
-import unittest
-from unittest.mock import patch, MagicMock
-from io import StringIO
-from console import HBNBCommand
-from models import storage
+#!/usr/bin/python3
+""" Console Module """
+import cmd
+import sys
+from models.base_model import BaseModel
+from models.__init__ import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
-class TestHBNBCommand(unittest.TestCase):
-    def setUp(self):
-        """Set up the HBNBCommand instance for testing."""
-        self.console = HBNBCommand()
-        self.patcher = patch('sys.stdout', new_callable=StringIO)
-        self.mock_stdout = self.patcher.start()
+class HBNBCommand(cmd.Cmd):
+    """ Contains the functionality for the HBNB console"""
 
-    def tearDown(self):
-        """Tear down after testing."""
-        self.patcher.stop()
+    # determines prompt for interactive/non-interactive modes
+    prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
 
-    @patch('sys.stdin', StringIO('quit\n'))
-    def test_quit(self):
-        """Test the quit command."""
-        with self.assertRaises(SystemExit):
-            self.console.cmdloop()
-        self.assertEqual(self.mock_stdout.getvalue(), '')
+    classes = {
+        'BaseModel': BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity,
+        'Review': Review
+    }
+    dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
+    types = {
+        'number_rooms': int, 'number_bathrooms': int,
+        'max_guest': int, 'price_by_night': int,
+        'latitude': float, 'longitude': float
+    }
 
-    @patch('sys.stdin', StringIO('EOF\n'))
-    def test_EOF(self):
-        """Test the EOF command."""
-        with self.assertRaises(SystemExit):
-            self.console.cmdloop()
-        self.assertEqual(self.mock_stdout.getvalue(), '\n')
+    def preloop(self):
+        """Prints if isatty is false"""
+        if not sys.__stdin__.isatty():
+            print('(hbnb)')
 
-    @patch('sys.stdin', StringIO('create BaseModel\nall\nEOF\n'))
-    def test_all(self):
-        """Test the all command."""
-        self.console.cmdloop()
-        self.assertIn('BaseModel', self.mock_stdout.getvalue())
+    def precmd(self, line):
+        """Reformat command line for advanced command syntax."""
+        _cmd = _cls = _id = _args = ''  # initialize line elements
 
-    @patch('sys.stdin', StringIO('create BaseModel\ncount BaseModel\nEOF\n'))
-    def test_count(self):
-        """Test the count command."""
-        self.console.cmdloop()
-        self.assertIn('1', self.mock_stdout.getvalue())
+        # scan for general formating - i.e '.', '(', ')'
+        if not ('.' in line and '(' in line and ')' in line):
+            return line
 
-    @patch('sys.stdin', StringIO('create BaseModel\n'
-                                 'show BaseModel 0000-0000-0000-0000\n'
-                                 'EOF\n'))
-    def test_show(self):
-        """Test the show command."""
-        self.console.cmdloop()
-        self.assertIn('BaseModel', self.mock_stdout.getvalue())
+        try:  # parse line left to right
+            pline = line[:]  # parsed line
 
-    @patch('sys.stdin', StringIO('create BaseModel\n'
-                                 'destroy BaseModel 0000-0000-0000-0000\n'
-                                 'all\n'
-                                 'EOF\n'))
-    def test_destroy(self):
-        """Test the destroy command."""
-        self.console.cmdloop()
-        self.assertNotIn('BaseModel', self.mock_stdout.getvalue())
+            # isolate <class name>
+            _cls = pline[:pline.find('.')]
 
-    @patch('sys.stdin', StringIO('create BaseModel\n'
-                                 'update BaseModel 0000-0000-0000-0000 '
-                                 'name "NewName"\n'
-                                 'show BaseModel 0000-0000-0000-0000\n'
-                                 'EOF\n'))
-    def test_update(self):
-        """Test the update command."""
-        self.console.cmdloop()
-        self.assertIn('NewName', self.mock_stdout.getvalue())
+            # isolate and validate <command>
+            _cmd = pline[pline.find('.') + 1:pline.find('(')]
+            if _cmd not in HBNBCommand.dot_cmds:
+                raise Exception
 
-    @patch('sys.stdin', StringIO('\n'))
-    def test_emptyline(self):
-        """Test the emptyline method."""
-        self.console.cmdloop()
-        self.assertEqual(self.mock_stdout.getvalue(), '')
+            # if parantheses contain arguments, parse them
+            pline = pline[pline.find('(') + 1:pline.find(')')]
+            if pline:
+                # partition args: (<id>, [<delim>], [<*args>])
+                pline = pline.partition(', ')  # pline convert to tuple
+
+                # isolate _id, stripping quotes
+                _id = pline[0].replace('\"', '')
+                # possible bug here:
+                # empty quotes register as empty _id when replaced
+
+                # if arguments exist beyond _id
+                pline = pline[2].strip()  # pline is now str
+                if pline:
+                    # check for *args or **kwargs
+                    if pline[0] is '{' and pline[-1] is '}'\
+                            and type(eval(pline)) is dict:
+                        _args = pline
+                    else:
+                        _args = pline.replace(',', '')
+                        # _args = _args.replace('\"', '')
+            line = ' '.join([_cmd, _cls, _id, _args])
+
+        except Exception as mess:
+            pass
+        finally:
+            return line
+
+    def postcmd(self, stop, line):
+        """Prints if isatty is false"""
+        if not sys.__stdin__.isatty():
+            print('(hbnb) ', end='')
+        return stop
+
+    def do_quit(self, command):
+        """ Method to exit the HBNB console"""
+        exit()
+
+    def help_quit(self):
+        """ Prints the help documentation for quit  """
+        print("Exits the program with formatting\n")
+
+    def do_EOF(self, arg):
+        """ Handles EOF to exit program """
+        print()
+        raise SystemExit
+
+    def help_EOF(self):
+        """ Prints the help documentation for EOF """
+        print("Exits the program without formatting\n")
+
+    def emptyline(self):
+        """ Overrides the emptyline method of CMD """
+        pass
+
+    def do_create(self, args):
+        """ Create an object of any class"""
+        if not args:
+            print("** class name missing **")
+            return
+        elif args not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = HBNBCommand.classes[args]()
+        storage.save()
+        print(new_instance.id)
+        storage.save()
+
+    def help_create(self):
+        """ Help information for the create method """
+        print("Creates a class of any type")
+        print("[Usage]: create <className>\n")
+
+    def do_show(self, args):
+        """ Method to show an individual object """
+        new = args.partition(" ")
+        c_name = new[0]
+        c_id = new[2]
+
+        # guard against trailing args
+        if c_id and ' ' in c_id:
+            c_id = c_id.partition(' ')[0]
+
+        if not c_name:
+            print("** class name missing **")
+            return
+
+        if c_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        if not c_id:
+            print("** instance id missing **")
+            return
+
+        key = c_name + "." + c_id
+        try:
+            print(storage._FileStorage__objects[key])
+        except KeyError:
+            print("** no instance found **")
+
+    def help_show(self):
+        """ Help information for the show command """
+        print("Shows an individual instance of a class")
+        print("[Usage]: show <className> <objectId>\n")
+
+    def do_destroy(self, args):
+        """ Destroys a specified object """
+        new = args.partition(" ")
+        c_name = new[0]
+        c_id = new[2]
+        if c_id and ' ' in c_id:
+            c_id = c_id.partition(' ')[0]
+
+        if not c_name:
+            print("** class name missing **")
+            return
+
+        if c_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        if not c_id:
+            print("** instance id missing **")
+            return
+
+        key = c_name + "." + c_id
+
+        try:
+            del (storage.all()[key])
+            storage.save()
+        except KeyError:
+            print("** no instance found **")
+
+    def help_destroy(self):
+        """ Help information for the destroy command """
+        print("Destroys an individual instance of a class")
+        print("[Usage]: destroy <className> <objectId>\n")
+
+    def do_all(self, args):
+        """ Shows all objects, or all objects of a class"""
+        print_list = []
+
+        if args:
+            args = args.split(' ')[0]  # remove possible trailing args
+            if args not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
+            for k, v in storage._FileStorage__objects.items():
+                if k.split('.')[0] == args:
+                    print_list.append(str(v))
+        else:
+            for k, v in storage._FileStorage__objects.items():
+                print_list.append(str(v))
+
+        print(print_list)
+
+    def help_all(self):
+        """ Help information for the all command """
+        print("Shows all objects, or all of a class")
+        print("[Usage]: all <className>\n")
+
+    def do_update(self, args):
+        """ Update an object based on its ID """
+        new = args.partition(" ")
+        c_name = new[0]
+        c_id = new[2]
+        args = new[2].partition(" ")
+        if args[2] and ' ' in args[2]:
+            args = args[2].partition(' ')
+            _id = args[0]
+            args = args[2]
+            if _id == 'id':
+                args = _id + args
+        else:
+            _id = args[0]
+            args = args[2]
+
+        # if arguments contain other than ' ', ',', '\"'
+        if args:
+            args = args.split(',')  # return as list
+            # if argument is comma-sep key:val
+            # else if argument is key val sep by space
+            args = [arg.split(' ') if ',' in arg else arg for arg in args]
+
+            # flatten args list to 1D
+            args = [elem for i in args for elem in (i if type(i) is list else [i])]
+            for i, arg in enumerate(args):
+                if '\"' in arg:
+                    args[i] = args[i].replace('\"', '')
+                if ',' in arg:
+                    args[i] = args[i].replace(',', '')
+        else:
+            print("** attribute name missing **")
+            return
+
+        if not c_name:
+            print("** class name missing **")
+            return
+
+        if c_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        if not c_id:
+            print("** instance id missing **")
+            return
+
+        if not args[0]:
+            print("** attribute name missing **")
+            return
+
+        if not args[1]:
+            print("** value missing **")
+            return
+
+        key = c_name + "." + c_id
+
+        try:
+            obj = storage._FileStorage__objects[key]
+
+        except KeyError:
+            print("** no instance found **")
+            return
+
+        for k, v in HBNBCommand.types.items():
+            if k in args[0]:
+                args[1] = v(args[1])
+        setattr(obj, args[0], args[1])
+        storage.save()
+
+    def help_update(self):
+        """ Help information for the update command """
+        print("Updates an instance based on its ID")
+        print("[Usage]: update <className> <objectId> <attributeName> \"<value>\"\n")
 
 
 if __name__ == '__main__':
-    unittest.main()
+    HBNBCommand().cmdloop()
