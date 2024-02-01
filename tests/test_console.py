9621b72807216bd64e475e33025a53398ca018b4
Eunice Adewusi <e.adewusi@alustudent.com>
2024-02-01 20:55:18 +0100

    some changes

--- tests/test_console.py
+++ tests/test_console.py
@@ -20,7 +20,6 @@ class ConsoleTestCase(unittest.TestCase):
         del self.stdout
         del self.storage
 
-<<<<<<< HEAD
     @patch('sys.stdin', StringIO('quit\n'))
     def test_quit(self):
         """Test the quit command."""
@@ -34,7 +33,6 @@ class ConsoleTestCase(unittest.TestCase):
         with self.assertRaises(SystemExit):
             self.console.cmdloop()
         self.assertEqual(self.mock_stdout.getvalue(), '(hbnb) \n')
-=======
     def test_create(self):
         """test create basic"""
         with patch('sys.stdout', self.stdout):
@@ -58,7 +56,6 @@ class ConsoleTestCase(unittest.TestCase):
             self.console.onecmd('create MyModel')
         self.assertEqual("** class doesn't exist **\n",
                          self.stdout.getvalue())
->>>>>>> ffc8a7d17c58d364e9b7c61adc6d15d51f6db793
 
     def test_all(self):
         """test all"""
