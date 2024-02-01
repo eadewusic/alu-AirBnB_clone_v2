8340a892b21f7f213ff1071fff3586abf9bb6ffb
Eunice Adewusi <e.adewusi@alustudent.com>
2024-02-01 22:21:10 +0100

    edit amenity.py file

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 """This is the amenity class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-from models.place import place_amenity
+from sqlalchemy.orm import relationship
+from models import storage_type
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
+    """This is the class for Amenity\
+    Defines blueprint for Amenity objects and table schema
     Attributes:
         name: input name
     """
     __tablename__ = "amenities"
+
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    if storage_type == 'db':
+        from models.place import place_amenity
+        place_amenities = relationship("Place",
+                                       secondary=place_amenity,
+                                       back_populates="amenities")
