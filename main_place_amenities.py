3edf93cd1f40af17b9d18bfc9f1ad3b2e41160b3
Eunice Adewusi <e.adewusi@alustudent.com>
2024-02-03 11:40:54 +0100

    Edit files

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -2,6 +2,11 @@
 """ Test link Many-To-Many Place <> Amenity
 """
 from models import *
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.user import User
 
 # creation of a State
 state = State(name="California")
